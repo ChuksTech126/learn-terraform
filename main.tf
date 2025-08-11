9e2f889f2b1f3e8686dd061d0742686036f10315
ChuksTech126 <chukzdigitalz@gmail.com>
2025-08-11 04:51:43 +0100

    configured VCS

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["al2023-ami-*-x86_64"] # Amazon Linux 2023 for x86_64
   }
 
   filter {
@@ -18,14 +18,14 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["137112412989"] # Amazon
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_instance" "amazon_linux" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.Instance_type
 
   tags = {
-    Name = var.instance_name
+    Name = var.Instance_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,16 +3,16 @@
 
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "us-east-1"
 }
 
-variable "instance_type" {
+variable "Instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
 
-variable "instance_name" {
+variable "Instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+  default     = "MyAmazonLinux2023"
 }
 
